b82669a85816e3cc1963038788bc34e12395ef13
MahsumKocabey <mahsumkocabey@hotmail.com>
2020-05-12 21:25:57 +0100

    Updates

---
+++ precache-manifest.619e996e7aaa755930ae0938f95462fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f75c13e607d7beb78b0a53d60f4b89bb",
+    "url": "/Ravenous/index.html"
+  },
+  {
+    "revision": "a9b41a1d1649dd02d638",
+    "url": "/Ravenous/static/css/main.1b4e2589.chunk.css"
+  },
+  {
+    "revision": "5b631a7ba56a5f56da04",
+    "url": "/Ravenous/static/js/2.70a0e2c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ravenous/static/js/2.70a0e2c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b41a1d1649dd02d638",
+    "url": "/Ravenous/static/js/main.24fe8485.chunk.js"
+  },
+  {
+    "revision": "9602e27de7a96bfdc4a6",
+    "url": "/Ravenous/static/js/runtime-main.44fb4812.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
