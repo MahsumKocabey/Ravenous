640c207c19a63d1f07bef7c94a9bb5a1ffb2f6f0
MahsumKocabey <mahsumkocabey@hotmail.com>
2020-05-12 18:54:40 +0100

    Search button created and some functiona are added

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import './BusinessList.css'
-import Business from '../Business/Business'
+import './BusinessList.css';
 
-class BusinessList extends React.Component{
-    render(){
-        return(
-            <div className="BusinessList">
-                {this.props.businesses.map(business =>{
-                    return <Business business={business} />
-                })}
-            </div>
-        )
-    }
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        {
+          this.props.businesses.map(business => {
+            return <Business business={business} />
+          })
+        }
+      </div>
+    );
+  }
 }
-
 
 export default BusinessList;
